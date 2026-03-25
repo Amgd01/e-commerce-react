92788a5624c79d64c98ff2fd33f6edd09eeed87d
Amgd <amgd_magdy108@yahoo.com>
2026-03-25 18:11:14 +0200

    Ready now for deployment

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from "path"
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base:"/[e-commerce-react]/",
+  base:"/e-commerce-react/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
